d682a37fc11931fe54f700a9d41a1a4ceb6cc491
Young Jun <yyj7736@naver.com>
2022-02-07 22:49:00 +0900

    그냥해봄

--- Project2/array.c
+++ Project2/array.c
@@ -3,24 +3,24 @@
 int main_array(void)
 {
 	// 배열
-	//int subway_1 = 30; // 지하철 1호차에 30명이 타고 있다
-	//int subway_2 = 40;
-	//int subway_3 = 50;
+	int subway_1 = 30; // 지하철 1호차에 30명이 타고 있다
+	int subway_2 = 40;
+	int subway_3 = 50;
 
-	//printf("지하철 1호차에 %d 명이 타고 있습니다.\n", subway_1);
-	//printf("지하철 2호차에 %d 명이 타고 있습니다.\n", subway_2);
-	//printf("지하철 3호차에 %d 명이 타고 있습니다.\n", subway_3);
+	printf("지하철 1호차에 %d 명이 타고 있습니다.\n", subway_1);
+	printf("지하철 2호차에 %d 명이 타고 있습니다.\n", subway_2);
+	printf("지하철 3호차에 %d 명이 타고 있습니다.\n", subway_3);
 
 	// 여러 개의 변수를 함께, 동시에 생성
-	//int subway_array[3]; // [0] [1] [2]
-	//subway_array[0] = 30;
-	//subway_array[1] = 40;
-	//subway_array[2] = 50;
+	int subway_array[3]; // [0] [1] [2]
+	subway_array[0] = 30;
+	subway_array[1] = 40;
+	subway_array[2] = 50;
 
-	//for (int i = 0; i < 3; i++)
-	//{
-	//	printf("지하철 %d호차에 %d 명이 타고 있습니다.\n", subway_array[i], i + 1);
-	//}
+	for (int i = 0; i < 3; i++)
+	{
+		printf("지하철 %d호차에 %d 명이 타고 있습니다.\n", subway_array[i], i + 1);
+	}
 
 	// 값 설정 방법
 	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 }; // arr[0] = 1, arr[1] = 2, ...
